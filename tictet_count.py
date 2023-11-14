0091133851c8d28160ee8c978c4254c3d9951957
litred <2418848397@qq.com>
2023-11-14 21:08:16 +0800

    1.0

--- tictet_count.py
+++ tictet_count.py
@@ -1,12 +1,26 @@
-result_dict = {x: 0 for x in int(100)}
-count = 0
+def input_value() -> tuple[dict, int]:
+    count = 0
+    result_dict = {}
+    while True:
+        name = input(f"{count + 1}: ") 
+        if isinstance(name, str):#防呆
+            if name == 'end':
+                break
+            else:
+                result_dict[name] = result_dict.get(name,0) + 1
+                count += 1
+        else:
+            print(f"请输入字母！\n")
+    return result_dict, count
 
-while True:
-    x = input(f"请输入第{count}张票的结果") 
-    if x == 'end':
-        break
-    else:
-        result_dict[x] += 1
-        count += 1
-#test
-print(f"统计结果如下\n共{count}张票")
+def print_output(result: dict, count: int):
+    print(f"\n统计结果如下\n共{count}张票\n")
+    for name, name_count in result.items():
+        print(f"{name}: {name_count}")
+       
+        
+if __name__ == "__main__":
+
+    print(f"请按顺序输入各张票的结果\n")
+    result_dict, count = input_value()
+    print_output(result_dict, count)
